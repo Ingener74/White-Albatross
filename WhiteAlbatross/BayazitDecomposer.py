e350e662d87aa15eaaf6e5212481a1ae057e608e
ShnaiderPavel <shnaiderpasha@gmail.com>
2015-08-14 16:29:28 +0300

    Добавил первый редуцент по Mark Bayazit's Algorithm :)

---
+++ WhiteAlbatross/BayazitDecomposer.py
@@ -0,0 +1,12 @@
+# encoding: utf8
+
+
+class BayazitDecomposer(object):
+    """
+    http://mpen.ca/406/bayazit
+    """
+    def __init__(self):
+        pass
+
+    def decompose(self, polygon):
+        return polygon
