3fa85d812538bb78785ff98108faa94b16924942
ShnaiderPavel <shnaiderpasha@gmail.com>
2015-08-19 11:16:15 +0300

    добавил функции для декомпозиции

--- WhiteAlbatross/BayazitDecomposer.py
+++ WhiteAlbatross/BayazitDecomposer.py
@@ -1,4 +1,45 @@
 # encoding: utf8
+import math
+
+
+def area(a, b, c):
+    return (b.x() - a.x())*(c.y() - a.y()) - ((c.x() - a.x())*(b.y() - a.y()))
+
+
+def left(a, b, c):
+    return area(a, b, c) > 0
+
+
+def left_on(a, b, c):
+    return area(a, b, c) >= 0
+
+
+def right(a, b, c):
+    return area(a, b, c) < 0
+
+
+def right_on(a, b, c):
+    return area(a, b, c) <= 0
+
+
+def collinear(a, b, c):
+    return area(a, b, c) == 0
+
+
+def sqdist(a, b):
+    return math.pow(b.x() - a.x(), 2) + math.pow(b.y() - a.y(), 2)
+
+
+def eq(a, b):
+    pass
+
+
+def wrap(a, b):
+    pass
+
+
+def srand(min, max):
+    pass
 
 
 class BayazitDecomposer(object):
@@ -9,4 +50,7 @@ class BayazitDecomposer(object):
         pass
 
     def decompose(self, polygon):
+
+
+
         return [[point for point in polygon]]
