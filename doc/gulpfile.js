282f2730b073f316212eaad3c4acfc7ea3b58431
ShnaiderPavel <shnaiderpasha@gmail.com>
2015-09-03 00:12:50 +0400

    поправил галп файл, деплой пока не работает

--- doc/gulpfile.js
+++ doc/gulpfile.js
@@ -5,69 +5,56 @@
 var gulp = require('gulp'),
     clean = require('gulp-clean'),
     connect = require('gulp-connect'),
-    runSequence = require('run-sequence'),
-    ghPages = require('gulp-gh-pages');
-
-var paths = {
-    app: 'app',
-    dist: 'build',
-    dist_js: 'build/js',
-    dist_css: 'build/css',
-    bootstrap: 'bower_components/bootstrap/dist'
-};
-
-var files = {
-    main: paths.app + '/index.html',
-    images: paths.app + '/*png',
-    js_files: '/js/*.min.js',
-    css_files: '/css/*.min.css'
-};
+    run_sequence = require('run-sequence'),
+    gh_pages = require('gulp-gh-pages');
 
 // Delete all files in dist
 gulp.task('clean', function () {
-    return gulp.src(paths.dist + '/*')
+    return gulp.src('./build/*')
         .pipe(clean());
 });
 
 gulp.task('deploy', function () {
-    return gulp.src(paths.dist + '/**/*')
-        .pipe(ghPages());
+    return gulp.src('./build/**/*')
+        .pipe(gh_pages({
+            force: true
+        }));
 });
 
 gulp.task('connect', function () {
     connect.server({
-        root: paths.dist,
+        root: './build',
         livereload: true
     });
 });
 
 // Copy html
 gulp.task('html', function () {
-    gulp.src(paths.app + '/*.html')
-        .pipe(gulp.dest(paths.dist))
+    gulp.src('./app/*.html')
+        .pipe(gulp.dest('./build'))
         .pipe(connect.reload());
 });
 
 // Copy java script files
 gulp.task('js', function () {
-    gulp.src(paths.bootstrap + files.js_files)
-        .pipe(gulp.dest(paths.dist + '/js'))
+    gulp.src('bower_components/bootstrap/dist/js/*.min.js')
+        .pipe(gulp.dest('./build/js'))
         .pipe(connect.reload());
 });
 
 // Copy css files
 gulp.task('css', function () {
-    gulp.src(paths.bootstrap + files.css_files)
-        .pipe(gulp.dest(paths.dist + '/css'))
+    gulp.src('bower_components/bootstrap/dist/css/*.min.css')
+        .pipe(gulp.dest('./build/css'))
         .pipe(connect.reload());
 });
 
 gulp.task('build', function (callback) {
-    runSequence('clean', ['html', 'js', 'css'], callback);
+    run_sequence('clean', ['html', 'js', 'css'], callback);
 });
 
 gulp.task('watch', function () {
-    gulp.watch([paths.app + '/*.html'], ['build'])
+    gulp.watch(['./app/*.html'], ['build'])
 });
 
 gulp.task('default', ['connect', 'build', 'watch']);
